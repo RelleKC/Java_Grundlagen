dd1b2e4d19fac74f2a1e0ccae419aae498a6bde2
RelleKC <38250362+rellekc@users.noreply.github.com>
2018-04-12 22:35:30 +0200

    add 'Aufgabe2'

--- Listen und Serialisierung/src/SupportObject.java
+++ Listen und Serialisierung/src/SupportObject.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SupportObject {
@@ -21,9 +23,72 @@ public class SupportObject {
 
 
     public static void main(String[] args) {
+        //Aufgabe 1
         SupportObject object = new SupportObject();
         System.out.println(object.toString());
-        System.out.println("Determinante: " + object.berechneDeterminante());
+        System.out.println("Determinante: " + object.berechneDeterminante() + '\n');
+
+        //Aufgabe 2
+        long start, end;
+
+        System.out.println("Zeit für das einfügen von 1000000 neu Instanziierten SupportObjects:");
+
+        ArrayList<SupportObject> objectArrayList = new ArrayList<SupportObject>();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            objectArrayList.add(new SupportObject());
+        }
+        end = System.currentTimeMillis();
+        printTimeDiff("ArrayList ", start, end);
+
+        LinkedList<SupportObject> objectLinkedList = new LinkedList<SupportObject>();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            objectLinkedList.add(new SupportObject());
+        }
+        end = System.currentTimeMillis();
+        printTimeDiff("LinkedList ", start, end);
+
+
+        System.out.println('\n' + "Zeit für die Determinantenberechnung aller Einträge:" + '\n');
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < objectArrayList.size(); i++) {
+            objectArrayList.get(i).berechneDeterminante();
+        }
+        end = System.currentTimeMillis();
+        printTimeDiff("ArrayList (for-Schleife)", start, end);
+
+
+        //LinkedList for-Schleife zu Zeitaufwändig
+        /*
+        start = System.currentTimeMillis();
+        for (int i = 0; i < objectLinkedList.size() ; i++) {
+            System.out.println(i);
+            objectLinkedList.get(i).berechneDeterminante();
+        }
+        end = System.currentTimeMillis();
+        printTimeDiff("LinkedList (for-Schleife)",start, end);
+        */
+
+
+        start = System.currentTimeMillis();
+        for (SupportObject tmp : objectArrayList) {
+            tmp.berechneDeterminante();
+        }
+        end = System.currentTimeMillis();
+        printTimeDiff("ArrayList (foreach-Schleife)", start, end);
+
+        start = System.currentTimeMillis();
+        for (SupportObject tmp : objectLinkedList) {
+            tmp.berechneDeterminante();
+        }
+        end = System.currentTimeMillis();
+        printTimeDiff("LinkedList (foreach-Schleife)", start, end);
+    }
+
+    public static void printTimeDiff(String name, long start, long end) {
+        System.out.println(name + (end - start) + "ms");
     }
 
     public int berechneDeterminante() {
